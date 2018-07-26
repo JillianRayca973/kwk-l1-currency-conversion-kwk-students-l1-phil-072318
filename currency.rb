e4845102d9aab14d07ce134ee00ec85dc8686ead
Jill  Rayca <rkrayca@yahoo.com>
2018-07-26 13:41:14 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,10 @@
 # Write your cod here
+def usd_to_eur(money)
+  puts "#{money * 0.86}"
+end
+usd_to_eur(100)
+
+def eur_to_usd(money)
+  puts "#{money * 1.16279069767500000000000001}"
+end
+eur_to_usd(86)
